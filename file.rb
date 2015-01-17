4d9e622b8b9e20aff36b04acba5332ea7c02f598
JD <jd@vizcaya.co>
2015-01-16 21:27:09 -0500

    Tercer commit

--- file.rb
+++ file.rb
@@ -8,13 +8,13 @@ puts "I could have code like this." # and the comment after is ignored
 # puts "This won't run."
 
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 puts "Is it true that 3 + 2 < 5 - 7?"
 
-puts 3 + 2 < 5 - 7
-
-puts "What is 3 + 2? #{3 + 2}"
-puts "What is 5 - 7? #{5 - 7}"
 
 puts "Oh, that's why it's false."
 
